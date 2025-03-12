e514da746daccdc7eabb74d6d7145d5ef2f881fb
JoseLuisMorenoGomez <jlmoreno@gmail.com>
2025-03-12 15:07:10 +0000

    otro commit

--- apps/heritage_buildings/admin.py
+++ apps/heritage_buildings/admin.py
@@ -1,95 +1,141 @@
 from django.contrib import admin
-from django import forms
-from django.contrib.gis.geos import Point
+from django.utils.translation import gettext_lazy as _
 from .models import (
-    ComunidadAutonoma, Provincia, Diocesis, RegistroPropiedad,
-    TipoDocumento, Documento, GradoProteccion, TipoAdquiriente, Edificio
+    ComunidadAutonoma, Provincia, Diocesis, TipoDocumento, Documento, RegistroPropiedad,
+    GradoProteccion, TipoAdquiriente, Edificio, Bibliografia, ReferenciaBibliografica
 )
-from .models import Edificio
 from .forms import EdificioForm
-# Inlines para los modelos relacionados
 
-class ProvinciaInline(admin.TabularInline):
-    model = Provincia
-    extra = 1
-    fields = ('nombre',)
 
-class EdificioInline(admin.TabularInline):
-    model = Edificio
-    extra = 1
-    fields = ('nombre', 'municipio', 'direccion')
-
+# Inline para Documento
 class DocumentoInline(admin.TabularInline):
     model = Documento
     extra = 1
-    fields = ('fecha', 'tipo', 'archivo', 'url')
+    fields = ("tipo", "fecha", "archivo", "url", "edificio")
+    raw_id_fields = ("edificio",)
 
 
-
-class ComunidadAutonomaAdmin(admin.ModelAdmin):
-    list_display = ('nombre',)
-    search_fields = ('nombre',)
-    inlines = [ProvinciaInline]
-
-class ProvinciaAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'comunidad_autonoma')
-    search_fields = ('nombre', 'comunidad_autonoma__nombre')
-    list_filter = ('comunidad_autonoma',)
-    inlines = [EdificioInline]
-
-class DiocesisAdmin(admin.ModelAdmin):
-    list_display = ('nombre',)
-    search_fields = ('nombre',)
-    inlines = [EdificioInline]
-
-class RegistroPropiedadAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'titular', 'telefono')
-    search_fields = ('nombre', 'titular')
-    inlines = [EdificioInline]
-
-class TipoDocumentoAdmin(admin.ModelAdmin):
-    list_display = ('denominacion',)
-    search_fields = ('denominacion',)
-    inlines = [DocumentoInline]
-
-class GradoProteccionAdmin(admin.ModelAdmin):
-    list_display = ('nombre',)
-    search_fields = ('nombre',)
-    inlines = [EdificioInline]
-
-class TipoAdquirienteAdmin(admin.ModelAdmin):
-    list_display = ('nombre',)
-    search_fields = ('nombre',)
-    inlines = [EdificioInline]
+# Inline para ReferenciaBibliografica
+class ReferenciaBibliograficaInline(admin.TabularInline):
+    model = ReferenciaBibliografica
+    extra = 1
+    fields = ("bibliografia", "volumen", "paginas")
+    raw_id_fields = ("bibliografia",)
 
 
+@admin.register(Edificio)
 class EdificioAdmin(admin.ModelAdmin):
-    form = EdificioForm  # Usar el formulario personalizado
-    list_display = ('nombre', 'municipio', 'provincia', 'comunidad_autonoma')
-    search_fields = ('nombre', 'municipio', 'provincia__nombre', 'comunidad_autonoma__nombre')
-    list_filter = ('comunidad_autonoma', 'provincia')
-    # Especificar explícitamente los campos que queremos en el formulario
-    fields = [
-        'nombre', 'municipio', 'provincia', 'comunidad_autonoma', 'direccion', 
-        'descripcion_registral', 'referencia_catastral', 'numero_finca', 'registro_propiedad', 
-        'diocesis', 'grado_proteccion', 'incluye_bienes_muebles', 'vendido', 'tipo_adquiriente', 
-        'fecha_venta', 'ciudad_venta', 'precio', 'notario', 'observaciones', 'coordenadas_texto'
-    ]
-    inlines = [DocumentoInline]
+    form = EdificioForm
+    list_display = (
+        "nombre", "municipio", "provincia", "comunidad_autonoma", "diocesis", "vendido", "desacralizado"
+    )
+    list_filter = ("provincia", "comunidad_autonoma", "vendido", "desacralizado")
+    search_fields = ("nombre", "municipio", "direccion", "referencia_catastral")
+    ordering = ("nombre",)
+    fieldsets = (
+        (_("Información General"), {
+            "fields": (
+                "nombre", "municipio", "provincia", "comunidad_autonoma", "diocesis", "direccion", "coordenadas_texto"
+            )
+        }),
+        (_("Información Registral"), {
+            "fields": (
+                "referencia_catastral", "registro_propiedad", "descripcion_registral", "notario"
+            )
+        }),
+        (_("Información Patrimonial"), {
+            "fields": (
+                "grado_proteccion", "incluye_bienes_muebles"
+            )
+        }),
+        (_("Venta y Adquisición"), {
+            "fields": (
+                "tipo_adquiriente", "fecha_venta", "precio", "ciudad_venta", "numero_finca"
+            )
+        }),
+        (_("Estado"), {
+            "fields": (
+                "vendido", "desacralizado"
+            )
+        }),
+        (_("Observaciones"), {
+            "fields": (
+                "observaciones",
+            )
+        }),
+    )
+    inlines = [DocumentoInline, ReferenciaBibliograficaInline]  # Asegúrate de que esté aquí
 
+
+# Registro de los demás modelos
+@admin.register(ComunidadAutonoma)
+class ComunidadAutonomaAdmin(admin.ModelAdmin):
+    list_display = ("nombre",)
+    search_fields = ("nombre",)
+
+
+@admin.register(Provincia)
+class ProvinciaAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "comunidad_autonoma")
+    list_filter = ("comunidad_autonoma",)
+    search_fields = ("nombre",)
+
+
+@admin.register(Diocesis)
+class DiocesisAdmin(admin.ModelAdmin):
+    list_display = ("nombre",)
+    search_fields = ("nombre",)
+
+
+@admin.register(TipoDocumento)
+class TipoDocumentoAdmin(admin.ModelAdmin):
+    list_display = ("denominacion",)
+    search_fields = ("denominacion",)
+
+
+@admin.register(Documento)
 class DocumentoAdmin(admin.ModelAdmin):
-    list_display = ('fecha', 'tipo', 'edificio')
-    search_fields = ('tipo__denominacion', 'edificio__nombre')
-    list_filter = ('tipo', 'edificio')
+    list_display = ("tipo", "fecha", "edificio", "archivo", "url")
+    search_fields = ("tipo__denominacion", "edificio__nombre")
+    list_filter = ("tipo", "fecha")
+    raw_id_fields = ("edificio",)
 
-# Registrar los modelos en el admin
 
-admin.site.register(ComunidadAutonoma, ComunidadAutonomaAdmin)
-admin.site.register(Provincia, ProvinciaAdmin)
-admin.site.register(Diocesis, DiocesisAdmin)
-admin.site.register(RegistroPropiedad, RegistroPropiedadAdmin)
-admin.site.register(TipoDocumento, TipoDocumentoAdmin)
-admin.site.register(GradoProteccion, GradoProteccionAdmin)
-admin.site.register(TipoAdquiriente, TipoAdquirienteAdmin)
-admin.site.register(Edificio,EdificioAdmin)
-admin.site.register(Documento, DocumentoAdmin)
+@admin.register(RegistroPropiedad)
+class RegistroPropiedadAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "titular", "telefono", "correo_electronico")
+    search_fields = ("nombre", "titular")
+    list_filter = ("titular",)
+
+
+@admin.register(GradoProteccion)
+class GradoProteccionAdmin(admin.ModelAdmin):
+    list_display = ("nombre",)
+    search_fields = ("nombre",)
+
+
+@admin.register(TipoAdquiriente)
+class TipoAdquirienteAdmin(admin.ModelAdmin):
+    list_display = ("nombre",)
+    search_fields = ("nombre",)
+
+
+@admin.register(Bibliografia)
+class BibliografiaAdmin(admin.ModelAdmin):
+    list_display = ("titulo", "autor", "tipo_identificador", "identificador", "url")
+    search_fields = ("titulo", "autor", "identificador")
+    list_filter = ("tipo_identificador",)
+
+
+@admin.register(ReferenciaBibliografica)
+class ReferenciaBibliograficaAdmin(admin.ModelAdmin):
+    list_display = ("bibliografia", "edificio", "volumen", "paginas")
+    search_fields = ("bibliografia__titulo", "edificio__nombre")
+    list_filter = ("edificio",)
+    raw_id_fields = ("edificio", "bibliografia")
+
+
+# Personalización del encabezado de Django Admin
+admin.site.site_header = _("Administración del Patrimonio")
+admin.site.site_title = _("Panel de Administración")
+admin.site.index_title = _("Gestión de Datos")
--- apps/heritage_buildings/models.py
+++ apps/heritage_buildings/models.py
@@ -6,7 +6,7 @@ from django.core.validators import FileExtensionValidator
 
 
 class ComunidadAutonoma(models.Model):
-    nombre = models.CharField(max_length=100, unique=True, verbose_name=_("Nombre"))
+    nombre = models.CharField(max_length=100, unique=True, verbose_name=_("Comunidad Autónoma"))
 
     def __str__(self):
         return self.nombre
@@ -16,192 +16,10 @@ class ComunidadAutonoma(models.Model):
         verbose_name_plural = _("Comunidades Autónomas")
 
 
-class Diocesis(models.Model):
-    nombre = models.CharField(max_length=100, unique=True, blank=True, verbose_name=_("Nombre"))
-
-    def __str__(self):
-        return self.nombre
-
-    class Meta:
-        verbose_name = _("Diócesis")
-        verbose_name_plural = _("Diócesis")
-
-
-class Documento(models.Model):
-    fecha = models.DateField(verbose_name=_("Fecha del Documento"))
-    tipo = models.ForeignKey(
-        "TipoDocumento",
-        on_delete=models.CASCADE,
-        related_name="documentos",
-        verbose_name=_("Tipo de Documento")
-    )
-    archivo = models.FileField(
-        upload_to="documents/%Y/%m/%d/",
-        validators=[FileExtensionValidator(allowed_extensions=["pdf", "txt", "doc", "docx", "odt", "rtf", "jpg", "mp4"])],
-        null=True,
-        blank=True,
-        verbose_name=_("Archivo (PDF,Texto,Foto o Video)")
-    )
-    url = models.URLField(null=True, blank=True, verbose_name=_("URL del Documento (opcional)"))
-    edificio = models.ForeignKey(
-        "Edificio",
-        on_delete=models.CASCADE,
-        related_name="documentos_relacionados",  # Cambiamos el related_name para evitar conflictos
-        verbose_name=_("Edificio"),
-        null=True,  
-        blank=True
-    )
-    grabado_por = models.ForeignKey(
-        User,
-        related_name="documentos_grabados",
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-        editable=False,
-        verbose_name=_("Grabado por")
-    )
-    fecha_grabacion = models.DateTimeField(auto_now_add=True, editable=False, verbose_name=_("Fecha de Grabación"))
-    modificado_por = models.ForeignKey(
-        User,
-        related_name="documentos_modificados",
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-        editable=False,
-        verbose_name=_("Modificado por")
-    )
-    fecha_modificacion = models.DateTimeField(auto_now=True, editable=False, verbose_name=_("Fecha de Modificación"))
-
-    def __str__(self):
-        return f"{self.tipo.denominacion} - {self.fecha}"
-
-    class Meta:
-        verbose_name = _("Documento")
-        verbose_name_plural = _("Documentos")
-        indexes = [models.Index(fields=["fecha", "tipo"])]
-
-
-class Edificio(models.Model):
-    nombre = models.CharField(max_length=200, verbose_name=_("Nombre del Edificio"))
-    comunidad_autonoma = models.ForeignKey(
-        "ComunidadAutonoma",
-        on_delete=models.CASCADE,
-        related_name="edificios",
-        verbose_name=_("Comunidad Autónoma")
-    )
-    provincia = models.ForeignKey(
-        "Provincia",
-        on_delete=models.CASCADE,
-        related_name="edificios",
-        verbose_name=_("Provincia")
-    )
-    municipio = models.CharField(max_length=100, verbose_name=_("Municipio"))
-    diocesis = models.ForeignKey(
-        "Diocesis",
-        on_delete=models.CASCADE,
-        related_name="edificios",
-        verbose_name=_("Diócesis"),
-        blank=True,
-        null=True
-    )
-    direccion = models.CharField(max_length=200, blank=True, null=True, verbose_name=_("Dirección"))
-    coordenadas = PointField(blank=True, null=True, verbose_name=_("Coordenadas (lat, lon)"))
-    referencia_catastral = models.CharField(
-        max_length=20,
-        unique=True,
-        blank=True,
-        null=True,
-        verbose_name=_("Referencia Catastral")
-    )
-    registro_propiedad = models.ForeignKey(
-        "RegistroPropiedad",
-        on_delete=models.CASCADE,
-        related_name="edificios",
-        verbose_name=_("Registro de Propiedad"),
-        blank=True,
-        null=True
-    )
-    numero_finca = models.CharField(max_length=20, blank=True, null=True, verbose_name=_("Número de Finca"))
-    descripcion_registral = models.TextField(blank=True, null=True, verbose_name=_("Descripción Registral"))
-    documentos = models.ManyToManyField(
-        "Documento",
-        related_name="edificios",
-        blank=True,
-        verbose_name=_("Documentos Asociados")
-    )
-    vendido = models.BooleanField(default=False, verbose_name=_("Vendido"))
-    fecha_venta = models.DateField(null=True, blank=True, verbose_name=_("Fecha de Venta"))
-    precio = models.DecimalField(
-        max_digits=10,
-        decimal_places=2,
-        null=True,
-        blank=True,
-        verbose_name=_("Precio (€)")
-    )
-    ciudad_venta = models.CharField(max_length=100, null=True, blank=True, verbose_name=_("Ciudad de Venta"))
-    notario = models.CharField(max_length=100, null=True, blank=True, verbose_name=_("Notario"))
-    incluye_bienes_muebles = models.BooleanField(default=False, verbose_name=_("Incluye Bienes Muebles"))
-    grado_proteccion = models.ForeignKey(
-        "GradoProteccion",
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-        related_name="edificios",
-        verbose_name=_("Grado de Protección")
-    )
-    tipo_adquiriente = models.ForeignKey(
-        "TipoAdquiriente",
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-        related_name="edificios",
-        verbose_name=_("Tipo de Adquiriente")
-    )
-    observaciones = models.TextField(blank=True, null=True, verbose_name=_("Observaciones"))
-    grabado_por = models.ForeignKey(
-        User,
-        related_name="edificios_grabados",
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-        editable=False,
-        verbose_name=_("Grabado por")
-    )
-    fecha_grabacion = models.DateTimeField(auto_now_add=True, editable=False, verbose_name=_("Fecha de Grabación"))
-    modificado_por = models.ForeignKey(
-        User,
-        related_name="edificios_modificados",
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-        editable=False,
-        verbose_name=_("Modificado por")
-    )
-    fecha_modificacion = models.DateTimeField(auto_now=True, editable=False, verbose_name=_("Fecha de Modificación"))
-
-    def __str__(self):
-        return f"{self.municipio} - {self.direccion}" if self.direccion else f"{self.municipio}"
-
-    class Meta:
-        verbose_name = _("Edificio")
-        verbose_name_plural = _("Edificios")
-
-
-class GradoProteccion(models.Model):
-    nombre = models.CharField(max_length=100, unique=True, verbose_name=_("Nombre"))
-
-    def __str__(self):
-        return self.nombre
-
-    class Meta:
-        verbose_name = _("Grado de Protección")
-        verbose_name_plural = _("Grados de Protección")
-
-
 class Provincia(models.Model):
-    nombre = models.CharField(max_length=100, unique=True, verbose_name=_("Nombre"))
+    nombre = models.CharField(max_length=100, unique=True, verbose_name=_("Provincia"))
     comunidad_autonoma = models.ForeignKey(
-        "ComunidadAutonoma",
+        ComunidadAutonoma,
         on_delete=models.CASCADE,
         related_name="provincias",
         verbose_name=_("Comunidad Autónoma")
@@ -213,6 +31,62 @@ class Provincia(models.Model):
     class Meta:
         verbose_name = _("Provincia")
         verbose_name_plural = _("Provincias")
+
+
+class Diocesis(models.Model):
+    nombre = models.CharField(max_length=100, unique=True, blank=True, verbose_name=_("Diócesis"))
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = _("Diócesis")
+        verbose_name_plural = _("Diócesis")
+
+
+class TipoDocumento(models.Model):
+    denominacion = models.CharField(max_length=100, unique=True, verbose_name=_("Denominación"))
+
+    def __str__(self):
+        return self.denominacion
+
+    class Meta:
+        verbose_name = _("Tipo de Documento")
+        verbose_name_plural = _("Tipos de Documentos")
+
+
+class Documento(models.Model):
+    fecha = models.DateField(verbose_name=_("Fecha del Documento"))
+    tipo = models.ForeignKey(
+        TipoDocumento,
+        on_delete=models.CASCADE,
+        related_name="documentos",
+        verbose_name=_("Tipo de Documento")
+    )
+    archivo = models.FileField(
+        upload_to="documents/%Y/%m/%d/",
+        validators=[FileExtensionValidator(allowed_extensions=["pdf", "txt", "doc", "docx", "odt", "rtf", "jpg", "mp4"])],
+        null=True,
+        blank=True,
+        verbose_name=_("Archivo (PDF, Texto, Foto o Video)")
+    )
+    url = models.URLField(null=True, blank=True, verbose_name=_("URL del Documento (opcional)"))
+    edificio = models.ForeignKey(
+        "Edificio",
+        on_delete=models.CASCADE,
+        related_name="documentos_relacionados",
+        verbose_name=_("Edificio"),
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return f"{self.tipo.denominacion} - {self.fecha}"
+
+    class Meta:
+        verbose_name = _("Documento")
+        verbose_name_plural = _("Documentos")
+        indexes = [models.Index(fields=["fecha", "tipo"])]
 
 
 class RegistroPropiedad(models.Model):
@@ -229,8 +103,19 @@ class RegistroPropiedad(models.Model):
         verbose_name_plural = _("Registros de Propiedad")
 
 
+class GradoProteccion(models.Model):
+    nombre = models.CharField(max_length=100, unique=True, verbose_name=_("Grado de Protección"))
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = _("Grado de Protección")
+        verbose_name_plural = _("Grados de Protección")
+
+
 class TipoAdquiriente(models.Model):
-    nombre = models.CharField(max_length=100, unique=True, verbose_name=_("Nombre"))
+    nombre = models.CharField(max_length=100, unique=True, verbose_name=_("Tipo de Adquiriente"))
 
     def __str__(self):
         return self.nombre
@@ -240,12 +125,122 @@ class TipoAdquiriente(models.Model):
         verbose_name_plural = _("Tipos de Adquirientes")
 
 
-class TipoDocumento(models.Model):
-    denominacion = models.CharField(max_length=100, unique=True, verbose_name=_("Denominación"))
-
+class Edificio(models.Model):
+    nombre = models.CharField(max_length=200, verbose_name=_("Nombre del Edificio"))
+    comunidad_autonoma = models.ForeignKey(
+        ComunidadAutonoma,
+        on_delete=models.CASCADE,
+        related_name="edificios",
+        verbose_name=_("Comunidad Autónoma")
+    )
+    provincia = models.ForeignKey(
+        Provincia,
+        on_delete=models.CASCADE,
+        related_name="edificios",
+        verbose_name=_("Provincia")
+    )
+    municipio = models.CharField(max_length=100, verbose_name=_("Municipio"))
+    diocesis = models.ForeignKey(
+        Diocesis,
+        on_delete=models.CASCADE,
+        related_name="edificios",
+        verbose_name=_("Diócesis"),
+        blank=True,
+        null=True
+    )
+    direccion = models.CharField(max_length=200, blank=True, null=True, verbose_name=_("Dirección"))
+    coordenadas = PointField(geography=True, blank=True, null=True, verbose_name=_("Coordenadas Geográficas"))
+    
+    
+    referencia_catastral = models.CharField(
+        max_length=20, unique=True, blank=True, null=True, verbose_name=_("Referencia Catastral")
+    )
+    
+    registro_propiedad = models.ForeignKey(
+        RegistroPropiedad,
+        on_delete=models.CASCADE,
+        related_name="edificios",
+        verbose_name=_("Registro de Propiedad"),
+        blank=True,
+        null=True
+    )
+    
+    descripcion_registral = models.TextField(blank=True, null=True, verbose_name=_("Descripción Registral"))
+    fecha_venta = models.DateField(blank=True, null=True, verbose_name=_("Fecha de Venta"))
+    precio = models.DecimalField(max_digits=12, decimal_places=2, blank=True, null=True, verbose_name=_("Precio de Venta"))
+    ciudad_venta = models.CharField(max_length=255, blank=True, null=True, verbose_name=_("Ciudad de Venta"))
+    numero_finca = models.CharField(max_length=50, blank=True, null=True, verbose_name=_("Número de Finca"))
+    notario = models.CharField(max_length=255, blank=True, null=True, verbose_name=_("Notario"))
+    
+    
+    grado_proteccion = models.ForeignKey(
+        GradoProteccion,
+        on_delete=models.SET_NULL,
+        related_name="edificios",
+        verbose_name=_("Grado de Protección"),
+        blank=True,
+        null=True
+    )
+    tipo_adquiriente = models.ForeignKey(
+        TipoAdquiriente,
+        on_delete=models.SET_NULL,
+        related_name="edificios",
+        verbose_name=_("Tipo de Adquiriente"),
+        blank=True,
+        null=True
+    )
+    incluye_bienes_muebles =  models.BooleanField(default=False, verbose_name=_("Incluye Bienes Muebles"))
+    desacralizado = models.BooleanField(default=False, verbose_name=_("Desacralizado"))
+    vendido = models.BooleanField(default=False, verbose_name=_("Vendido"))
+    observaciones = models.TextField(blank=True, null=True, verbose_name=_("Observaciones"))
+    
     def __str__(self):
-        return self.denominacion
+        return f"{self.municipio} - {self.direccion}" if self.direccion else f"{self.municipio}"
 
     class Meta:
-        verbose_name = _("Tipo de Documento")
-        verbose_name_plural = _("Tipos de Documentos")
+        verbose_name = _("Edificio")
+        verbose_name_plural = _("Edificios")
+
+
+# NUEVOS MODELOS PARA BIBLIOGRAFÍA Y REFERENCIAS
+
+class Bibliografia(models.Model):
+    TIPO_IDENTIFICADOR_CHOICES = [
+        ('ISBN', 'ISBN'),
+        ('DOI', 'DOI'),
+        ('ISSN', 'ISSN'),
+    ]
+
+    titulo = models.CharField(max_length=255, verbose_name=_("Título"))
+    autor = models.CharField(max_length=255, verbose_name=_("Autor(es)"))
+    tipo_identificador = models.CharField(
+        max_length=10, choices=TIPO_IDENTIFICADOR_CHOICES, verbose_name=_("Tipo de Identificador")
+    )
+    identificador = models.CharField(max_length=100, unique=True, verbose_name=_("Identificador"))
+    url = models.URLField(blank=True, null=True, verbose_name=_("URL"))
+
+    def __str__(self):
+        return f"{self.titulo} - {self.autor}"
+
+    class Meta:
+        verbose_name = _("Bibliografía")
+        verbose_name_plural = _("Bibliografías")
+
+
+class ReferenciaBibliografica(models.Model):
+    edificio = models.ForeignKey(
+        Edificio, on_delete=models.CASCADE, related_name="referencias_bibliograficas", verbose_name=_("Edificio")
+    )
+    bibliografia = models.ForeignKey(
+        Bibliografia, on_delete=models.CASCADE, related_name="referencias", verbose_name=_("Bibliografía")
+    )
+    volumen = models.CharField(max_length=50, blank=True, null=True, verbose_name=_("Volumen"))
+    paginas = models.CharField(max_length=50, blank=True, null=True, verbose_name=_("Intervalo de Páginas"))
+
+    def __str__(self):
+        return f"{self.bibliografia.titulo} - {self.edificio.nombre}"
+
+    class Meta:
+        verbose_name = _("Referencia Bibliográfica")
+        verbose_name_plural = _("Referencias Bibliográficas")
+
