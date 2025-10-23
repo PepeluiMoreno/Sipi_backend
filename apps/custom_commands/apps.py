d738bc5c148f9d569eb147c81252a471f07a303c
PepeluiMoreno <joseluis.morgomez@gmail.com>
2025-10-23 17:43:17 +0200

    pasando de django a fastapi + sqlalchemy

--- apps/custom_commands/apps.py
+++
@@ -1,7 +0,0 @@
-# heritage_defense/apps/custom_commands/apps.py
-
-from django.apps import AppConfig
-
-class buildingsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.custom_commands'
--- apps/heritage_buildings/apps.py
+++
@@ -1,7 +0,0 @@
-# heritage_defense/apps/heritage_buildings/apps.py
-
-from django.apps import AppConfig
-
-class buildingsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.heritage_buildings'
